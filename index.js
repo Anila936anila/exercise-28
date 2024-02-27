e8dd339ca58fe18fb3ef5ac3a24da6b00fd02791
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 13:01:08 -0800

    Stage of life
    Typescript 45Q assignment exercise 28

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+let age = 25;
+// Less then 2 years old, print a message that the person is a body.
+if (age < 2) {
+    console.log("The person is a baby.");
+}
+else if (age < 4) {
+    console.log("The person is a toddler.");
+}
+else if (age < 13) {
+    console.log("The person is a kid.");
+}
+else if (age < 20) {
+    console.log("The person is a teenager.");
+}
+else if (age < 65) {
+    console.log("The person is a adult.");
+}
+else {
+    console.log("The person is a elder.");
+}
